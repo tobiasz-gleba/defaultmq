7a1223a72dadd907b3131de3a33dee01ebe7fafd
tobiasz-gleba <tobiasz.gleba@efitness.com>
2022-05-14 16:32:00 +0200

    redis cache debuged

--- src/app/database/cache/memcache.py
+++ src/app/database/cache/memcache.py
@@ -1,13 +1,15 @@
+from random import expovariate
 from config import config
 from memcache import Client
+#TODO implement memcached
 
 class MemcacheCache():
     
     def __init__(self):
-        self.connection = Client([f'{config.cache_settings.URL}:{config.cache_settings.PORT}'])
+        self.connection = Client([f'{config.cache_settings.CACHE_URL}:{config.cache_settings.CACHE_PORT}'])
 
     def get(self, cache_key):
         self.connection.get(cache_key)
 
-    def set(self, cache_key):
-        self.connection.set(cache_key)
+    def set(self, cache_key, value, expire=-1):
+        self.connection.set(cache_key, value, expire)
--- src/app/database/cache/redis.py
+++ src/app/database/cache/redis.py
@@ -3,10 +3,10 @@ from config import config
 class RedisCache():
     
     def __init__(self):
-        self.connection = Redis(host=config.cache_settings.URL, port=config.cache_settings.PORT)
+        self.connection = Redis(host=config.cache_settings.CACHE_URL, port=config.cache_settings.CACHE_PORT)
 
     def get(self, cache_key):
         self.connection.get(cache_key)
 
-    def set(self, cache_key):
-        self.connection.set(cache_key)
+    def set(self, cache_key, value, ex=-1):
+        self.connection.set(cache_key, value, ex=ex)
